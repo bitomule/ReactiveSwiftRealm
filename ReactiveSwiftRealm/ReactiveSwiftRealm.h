3a9abfa3f5af7d5e1ed3eab347256378fc6683dd
David Collado <bitomule@gmail.com>
2017-01-18 10:47:29 +0100

    Project setup

---
+++ ReactiveSwiftRealm/ReactiveSwiftRealm.h
@@ -0,0 +1,19 @@
+//
+//  ReactiveSwiftRealm.h
+//  ReactiveSwiftRealm
+//
+//  Created by David Collado on 18/1/17.
+//  Copyright © 2017 David Collado. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ReactiveSwiftRealm.
+FOUNDATION_EXPORT double ReactiveSwiftRealmVersionNumber;
+
+//! Project version string for ReactiveSwiftRealm.
+FOUNDATION_EXPORT const unsigned char ReactiveSwiftRealmVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ReactiveSwiftRealm/PublicHeader.h>
+
+
