510ac8b5a5f701584e3e767cff8100e4c4a56f2e
David Collado <bitomule@gmail.com>
2017-01-24 10:56:28 +0100

    clean

---
+++ ReactiveSwiftRealm-macOS/ReactiveSwiftRealm-macOS.h
@@ -0,0 +1,19 @@
+//
+//  ReactiveSwiftRealm-macOS.h
+//  ReactiveSwiftRealm-macOS
+//
+//  Created by David Collado on 24/1/17.
+//  Copyright © 2017 David Collado. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for ReactiveSwiftRealm-macOS.
+FOUNDATION_EXPORT double ReactiveSwiftRealm_macOSVersionNumber;
+
+//! Project version string for ReactiveSwiftRealm-macOS.
+FOUNDATION_EXPORT const unsigned char ReactiveSwiftRealm_macOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ReactiveSwiftRealm_macOS/PublicHeader.h>
+
+
